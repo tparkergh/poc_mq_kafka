03ae335848463026f7ca147e82dae819c996f5ee
David R. Smith <David.Smith@osi.ca.gov>
2019-04-02 14:42:40 -0700

    SNAP-989: SPIKE: Research MQ Event Consumer and Kafka deployment

---
+++ src/main/java/gov/poc/demo/Consumer.java
@@ -0,0 +1,22 @@
+package gov.poc.demo;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class Consumer {
+
+  @KafkaListener(topics = {"replication"})
+  public void listen(ConsumerRecord<?, ?> record) throws Exception {
+
+    Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+
+    if (kafkaMessage.isPresent()) {
+      Object message = kafkaMessage.get();
+    }
+
+  }
+}
